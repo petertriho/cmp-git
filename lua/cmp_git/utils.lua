78c2be54776e2a099d31fcb31d286bbf3fc40273
Peter Tri Ho <mail@petertriho.com>
2021-10-25 19:18:21 +1100

    refactor: simplify utils.get_git_info

--- lua/cmp_git/utils.lua
+++ lua/cmp_git/utils.lua
@@ -1,26 +1,17 @@
 local M = {}
 
-M.get_gitlab_info = function(url)
-    local clean_url = string.gsub(url, "%.git", "")
-    return string.match(clean_url, "(gitlab).*[/:](.+)/(.+)$")
-end
-
-M.get_github_info = function(url)
-    local clean_url = string.gsub(url, "%.git", "")
-    return string.match(clean_url, "(github)%.com[/:](.+)/(.+)$")
-end
-
 M.get_git_info = function()
     local remote_origin_url = vim.fn.system("git config --get remote.origin.url")
+    local clean_remote_origin_url = string.gsub(remote_origin_url, "%.git", "")
     local host, owner, repo
 
     local is_gitlab_repo = string.find(remote_origin_url, "gitlab")
     local is_github_repo = string.find(remote_origin_url, "github")
 
     if is_github_repo then
-        host, owner, repo = M.get_github_info(remote_origin_url)
+        host, owner, repo = string.match(clean_remote_origin_url, "(github)%.com[/:](.+)/(.+)$")
     elseif is_gitlab_repo then
-        host, owner, repo = M.get_gitlab_info(remote_origin_url)
+        host, owner, repo = string.match(clean_remote_origin_url, "(gitlab).*[/:](.+)/(.+)$")
     end
 
     return { host = host, owner = owner, repo = repo }
