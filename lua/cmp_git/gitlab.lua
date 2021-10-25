45778887829ded8e7d53d47435f32df12e9a3750
Peter Tri Ho <mail@petertriho.com>
2021-10-25 23:29:33 +1100

    feat: add curl issues/mentions for gitlab #2

--- lua/cmp_git/gitlab.lua
+++ lua/cmp_git/gitlab.lua
@@ -1,54 +1,68 @@
 local Job = require("plenary.job")
+local utils = require("cmp_git.utils")
 
 local M = {}
 
 M.get_issues = function(source, callback, bufnr, owner, repo)
     local command = nil
-    local used_glab = false
 
-    if vim.fn.executable("glab") == 1 then
+    -- if vim.fn.executable("glab") == 1 then
+    --     command = {
+    --         "glab",
+    --         "api",
+    --         string.format(
+    --             "/projects/:id/issues?per_page=%d&state=%s",
+    --             source.config.gitlab.issues.limit,
+    --             source.config.gitlab.issues.state
+    --         ),
+    --     }
+    if vim.fn.executable("curl") == 1 then
+        local url = string.format(
+            "https://gitlab.com/api/v4/projects/%s/issues?per_page=%d&state=%s",
+            utils.url_encode(string.format("%s/%s", owner, repo)),
+            source.config.gitlab.issues.limit,
+            source.config.gitlab.issues.state
+        )
+
         command = {
-            "glab",
-            "api",
-            string.format(
-                "/projects/:id/issues?per_page=%d&state=%s",
-                source.config.gitlab.issues.limit,
-                source.config.gitlab.issues.state
-            ),
+            "curl",
+            url,
         }
 
-        used_glab = true
+        if vim.fn.exists("$GITLAB_TOKEN") == 1 then
+            local token = vim.fn.getenv("GITLAB_TOKEN")
+            local authorization_header = string.format("Authorization: Bearer %s", token)
+            table.insert(command, "-H")
+            table.insert(command, authorization_header)
+        end
     else
-        vim.notify("glab executables not found!")
+        vim.notify("glab and curl executables not found!")
         return
     end
 
     command.on_exit = function(job)
-        -- TODO: check for empty result?
         local result = job:result()
 
         local ok, parsed = pcall(vim.json.decode, table.concat(result, ""))
         if not ok then
-            vim.notify("Failed to parse github api result")
+            vim.notify("Failed to parse gitlab api result")
             return
         end
 
         local items = {}
 
-        if used_glab then
-            for _, issue in ipairs(parsed) do
-                if issue.description == vim.NIL then
-                    issue.description = ""
-                end
-
-                table.insert(items, {
-                    label = string.format("#%s", issue.iid),
-                    documentation = {
-                        kind = "markdown",
-                        value = string.format("# %s\n\n%s", issue.title, issue.description),
-                    },
-                })
+        for _, issue in ipairs(parsed) do
+            if issue.description == vim.NIL then
+                issue.description = ""
             end
+
+            table.insert(items, {
+                label = string.format("#%s", issue.iid),
+                documentation = {
+                    kind = "markdown",
+                    value = string.format("# %s\n\n%s", issue.title, issue.description),
+                },
+            })
         end
 
         callback({ items = items, isIncomplete = false })
@@ -61,48 +75,60 @@ end
 
 M.get_mentions = function(source, callback, bufnr, owner, repo)
     local command = nil
-    local used_glab = false
 
-    if vim.fn.executable("glab") == 1 then
+    -- if vim.fn.executable("glab") == 1 then
+    --     command = {
+    --         "glab",
+    --         "api",
+    --         string.format("/projects/:id/users?per_page=%d", source.config.gitlab.mentions.limit),
+    --     }
+    if vim.fn.executable("curl") == 1 then
+        local url = string.format(
+            "https://gitlab.com/api/v4/projects/%s/users?per_page=%d",
+            utils.url_encode(string.format("%s/%s", owner, repo)),
+            source.config.gitlab.mentions.limit
+        )
+
         command = {
-            "glab",
-            "api",
-            string.format("/projects/:id/users?per_page=%d", source.config.gitlab.mentions.limit),
+            "curl",
+            url,
         }
 
-        used_glab = true
+        if vim.fn.exists("$GITLAB_TOKEN") == 1 then
+            local token = vim.fn.getenv("GITLAB_TOKEN")
+            local authorization_header = string.format("Authorization: Bearer %s", token)
+            table.insert(command, "-H")
+            table.insert(command, authorization_header)
+        end
     else
         vim.notify("glab executables not found!")
         return
     end
 
     command.on_exit = function(job)
-        -- TODO: check for empty result?
         local result = job:result()
 
         local ok, parsed = pcall(vim.json.decode, table.concat(result, ""))
         if not ok then
-            vim.notify("Failed to parse github api result")
+            vim.notify("Failed to parse gitlab api result")
             return
         end
 
         local items = {}
 
-        if used_glab then
-            for _, mention in ipairs(parsed) do
-                table.insert(items, {
-                    label = string.format("@%s", mention.username),
-                    documentation = {
-                        kind = "markdown",
-                        value = string.format("# %s\n\n%s", mention.username, mention.name),
-                    },
-                })
-            end
+        for _, mention in ipairs(parsed) do
+            table.insert(items, {
+                label = string.format("@%s", mention.username),
+                documentation = {
+                    kind = "markdown",
+                    value = string.format("# %s\n\n%s", mention.username, mention.name),
+                },
+            })
         end
 
         callback({ items = items, isIncomplete = false })
 
-        source.cache_issues[bufnr] = items
+        source.cache_mentions[bufnr] = items
     end
 
     Job:new(command):start()
--- lua/cmp_git/utils.lua
+++ lua/cmp_git/utils.lua
@@ -1,8 +1,19 @@
 local M = {}
 
+local char_to_hex = function(c)
+    return string.format("%%%02X", string.byte(c))
+end
+
+M.url_encode = function(value)
+    return string.gsub(value, "([^%w _%%%-%.~])", char_to_hex)
+end
+
 M.get_git_info = function()
     local remote_origin_url = vim.fn.system("git config --get remote.origin.url")
+
     local clean_remote_origin_url = string.gsub(remote_origin_url, "%.git", "")
+    clean_remote_origin_url = string.gsub(clean_remote_origin_url, "%s", "")
+
     local host, owner, repo
 
     local is_gitlab_repo = string.find(remote_origin_url, "gitlab")
